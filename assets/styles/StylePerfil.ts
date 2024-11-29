9582e0289367b6a656c961971145d216680d1df3
JorgeRc1 <chowskith@gmail.com>
2024-11-28 18:04:49 -0600

    interfaces para el Perfil

---
+++ assets/styles/StylePerfil.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const stylesPerfil = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+      },
+      header: {
+        alignItems: "center",
+        marginVertical: 20,
+      },
+      avatar: {
+        backgroundColor: "#D3D3D3",
+      },
+      name: {
+        marginTop: 10,
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+      infoSection: {
+        marginHorizontal: 10,
+        marginVertical: 20,
+      },
+      label: {
+        color: "#555",
+        fontWeight: "bold",
+      },
+      ButtonSesion: {
+        backgroundColor: "#00A850",
+        marginHorizontal: 20,
+        marginVertical: 10,
+        marginBottom: 30,
+        borderRadius: 8,
+      },
+
+ });
+
+ export default stylesPerfil;
--- screens/user/Perfil.tsx
+++ screens/user/Perfil.tsx
@@ -1,14 +1,90 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { Avatar, Text, ListItem, Button, Icon } from "@rneui/base";
+import stylesPerfil from "@/assets/styles/StylePerfil";
 
 const Perfil = () => {
-    return (
-    <View>
-        <Text>
-            page perfil
-        </Text>
 
-    </View>
+    const user = {
+        nombre: "Jhon Z",
+        apellido: "Melendez",
+        email: "johndoe@example.com",
+        telefono: "1234567890",
+        fecha_create: new Date,
+        fecha_update: new Date
+    }
+    return (
+        <ScrollView style={stylesPerfil.container}>
+            {/* Encabezado con Avatar */}
+            <View style={stylesPerfil.header}>
+                <Avatar
+                    rounded
+                    size="xlarge"
+                    title={`${user.nombre[0]}${user.apellido[0]}`}
+                    containerStyle={stylesPerfil.avatar}
+                />
+                <Text h3 style={stylesPerfil.name}>
+                    {user.nombre} {user.apellido}
+                </Text>
+            </View>
+
+            {/* Información del Usuario */}
+            <View style={stylesPerfil.infoSection}>
+                <ListItem bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title style={stylesPerfil.label}>Teléfono</ListItem.Title>
+                        <ListItem.Subtitle>
+                            {user.telefono ? user.telefono : "No proporcionado"}
+                        </ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+
+                <ListItem bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title style={stylesPerfil.label}>Correo Electrónico</ListItem.Title>
+                        <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+
+                <ListItem bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title style={stylesPerfil.label}>Contraseña</ListItem.Title>
+                        <Button
+                            titleStyle={{ color: '#28A745' }}
+                            type="clear"
+                        >Cambiar
+                            <Icon name="lock-reset" color="#00A850" />
+                        </Button>
+                    </ListItem.Content>
+                </ListItem>
+
+                <ListItem bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title style={stylesPerfil.label}>Creado el</ListItem.Title>
+                        <ListItem.Subtitle>
+                            {new Date(user.fecha_create).toLocaleDateString()}
+                        </ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+
+                <ListItem bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title style={stylesPerfil.label}>Actualizado el</ListItem.Title>
+                        <ListItem.Subtitle>
+                            {new Date(user.fecha_update).toLocaleDateString()}
+                        </ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+            </View>
+
+            {/* Botón para Cerrar la sesion */}
+            <Button
+                title="Cerrar Sesion"
+                onPress={() => alert("Editar perfil")}
+                buttonStyle={stylesPerfil.ButtonSesion}
+            />
+
+        </ScrollView>
     );
 
 }
